c622d65d733daa84c6d2ef39498160de7fee3b1f
boo4ce <nguyenvietanh1482k@gmail.com>
2021-07-09 12:02:24 +0700

    finished push update

--- proxads/src/main/java/com/proxglobal/proxads/ProxUtils.java
+++ proxads/src/main/java/com/proxglobal/proxads/ProxUtils.java
@@ -1,15 +1,23 @@
 package com.proxglobal.proxads;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.afollestad.materialdialogs.LayoutMode;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.bottomsheets.BottomSheet;
+import com.afollestad.materialdialogs.bottomsheets.BuildConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.gson.Gson;
@@ -29,6 +37,7 @@ public class ProxUtils {
 
     public static final String TEST_INTERSTITIAL_ID = "ca-app-pub-3940256099942544/1033173712";
     public static final String TEST_NATIVE_ID = "ca-app-pub-3940256099942544/2247696110";
+    public static final String PREF_RATE = "PREF_RATE";
 
     public ProxInterstitialAd createInterstitialAd (Activity activity, String adId) {
         return new ProxInterstitialAd(activity, adId);
@@ -55,7 +64,6 @@ public class ProxUtils {
         config.fetchAndActivate().addOnCompleteListener(task -> {
             if (!task.isSuccessful()) return;
             String json = config.getString("config_update_version");
-            Log.d(TAG, "RemoteConfig: " + json);
 
             ConfigUpdateVersion result = new Gson().fromJson(json, ConfigUpdateVersion.class);
 
@@ -77,9 +85,51 @@ public class ProxUtils {
     }
 
     private void showBottomSheetUpdate(Activity activity, ConfigUpdateVersion config) {
-        new MaterialDialog(activity, new BottomSheet(LayoutMode.WRAP_CONTENT)).show();
+        MaterialDialog dialog = new MaterialDialog(activity, new BottomSheet(LayoutMode.WRAP_CONTENT));
+        dialog.setContentView(R.layout.bottom_remote_update);
+
+        ((TextView) dialog.findViewById(R.id.bru_title)).setText(config.title);
+        ((TextView) dialog.findViewById(R.id.bru_version_name)).setText(config.versionName);
+        ((TextView) dialog.findViewById(R.id.bru_message)).setText(config.message);
+        ((Button) dialog.findViewById(R.id.bru_update)).setOnClickListener(v ->{
+            linkToStore(activity, config.newPackage);
+            dialog.cancel();
+
+        });
     }
 
+    private void linkToStore(Activity activity, String newPackage) {
+        SharedPreferences sharedPreferences = activity.getSharedPreferences(
+                PREF_RATE, Context.MODE_PRIVATE
+        );
 
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putBoolean(PREF_RATE, true);
+        editor.apply();
+
+        String appPackageName = "";
+        if (newPackage.equals("")) {
+            appPackageName = activity.getPackageName();
+        } else {
+            appPackageName = newPackage;
+        }
+
+        try {
+            activity.startActivity(
+                     new Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + appPackageName)
+                    )
+            );
+        } catch (ActivityNotFoundException e) {
+            activity.startActivity(
+                     new Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)
+                    )
+            );
+        }
+    }
 
 }
